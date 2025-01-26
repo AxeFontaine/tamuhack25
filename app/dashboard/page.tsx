a4542775e6bb5cb29dedbec843c96800141491d0
Axel Fontaine <22axfontaine@gmail.com>
2025-01-26 03:32:48 -0600

    Updated dashboard logic

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,7 +7,7 @@ const Dashboard = () => {
   const currUser = false;
   const [currActivity, setCurrActivity] = useState(1);
 
-  const ActivityButton: React.FC<{ idx: number}> = ({ idx }) => {
+  const ActivityButton: React.FC<{ idx: number }> = ({ idx }) => {
     let position;
     switch (idx % 3) {
       case 0:
@@ -35,14 +35,15 @@ const Dashboard = () => {
     }
 
     const handleClick = () => {
-        if (idx + 1 <= currActivity) {
-            setCurrActivity(currActivity + 1);
-            console.log(currActivity);
-        }
-    }
+      setCurrActivity(currActivity + 1);
+    };
 
     return (
-      <button onClick={handleClick} className={`w-1/4 h-24 ${bg_color} rounded-xl mx-2 ${position} shrink-0 flex justify-center items-center`}>
+      <button
+        onClick={handleClick}
+        className={`w-1/4 h-24 ${bg_color} rounded-xl mx-2 ${position} shrink-0 flex justify-center items-center`}
+        disabled={currActivity != idx + 1}
+      >
         <Image src={file_name} alt="" width={60} height={40} />
       </button>
     );
@@ -51,11 +52,18 @@ const Dashboard = () => {
   return (
     <div className="h-full w-full flex bg-[#2F394D]">
       <div className="w-3/4 h-full flex flex-col mx-12 overflow-y-scroll">
-        <h2 className="w-full text-center border-b-2 border-[#EEE1B3] leading-[0.1em] my-12">
-          <span className="bg-[#2F394D] p-2 font-bold text-[#EEE1B3] text-2xl leading-[0.1em]"> LESSON 1 </span>
-        </h2>
-        {[...Array(10)].map((x, i) => (
-          <ActivityButton key={i} idx={i} />
+        {[...Array(5)].map((_, i) => (
+          <div key={i}>
+            <h2 className="w-full text-center border-b-2 border-[#EEE1B3] leading-[0.1em] my-12">
+              <span className="bg-[#2F394D] p-2 font-bold text-[#EEE1B3] text-2xl leading-[0.1em]">
+                {' '}
+                LESSON {i + 1}{' '}
+              </span>
+            </h2>
+            {[...Array(7)].map((_, j) => (
+              <ActivityButton key={j} idx={j + i * 7} />
+            ))}
+          </div>
         ))}
       </div>
       <div className="w-1/2 h-full flex flex-col items-center justify-around">
@@ -73,7 +81,7 @@ const Dashboard = () => {
               Novice
             </div>
             <div className="font-bold text-2xl text-[#2CDE9A] flex flex-row gap-2 items-center">
-              <Image src="flame.svg" alt="" width={30} height={30} />2
+              <Image src="flame.svg" alt="" width={30} height={30} />0
             </div>
           </div>
 
@@ -82,8 +90,11 @@ const Dashboard = () => {
             <div className="text-[#EEE1B3] font-bold text-md pl-5">
               Finish 5 activities
               <div className="relative w-full h-5 bg-slate-500 rounded-lg">
-                <p className="absolute h-full left-0 right-0 me-auto ms-auto w-fit">{currActivity}/5</p>
-                <div className="h-full bg-[#D8A1CA] rounded-lg max-w-full" style={{ width: `${(currActivity / 5) * 100}%` }}></div>
+                <p className="absolute h-full left-0 right-0 me-auto ms-auto w-fit">{currActivity - 1}/5</p>
+                <div
+                  className="h-full bg-[#D8A1CA] rounded-lg max-w-full"
+                  style={{ width: `${((currActivity - 1) / 5) * 100}%` }}
+                ></div>
               </div>
             </div>
           </div>
