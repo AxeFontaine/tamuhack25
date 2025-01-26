95fc10853bfc361b957ec3e01fd10841489e6f1a
Axel Fontaine <22axfontaine@gmail.com>
2025-01-26 09:53:00 -0600

    Fix Nav Bar merge issue

--- app/_components/NavigationBar.jsx
+++ app/_components/NavigationBar.jsx
@@ -1,10 +1,11 @@
 export default function NavigationBar() {
     return (
-        <div className="bg-[#111621] h-16 content-center flex items-center">
-            <a href="/" className="p-5 text-white">Logo</a>
-            <a href="/learn" className="p-5 duration-300 ease-in-out hover:bg-[#304165] hover:text-white">Learn</a>
+        <div className="bg-[#111621] h-16 content-center flex items-center text-white">
+            <a href="/" className="p-5 text-white">
+                <img src="/icon.png" alt="Logo" className="h-8 bg-white rounded-full" />
+            </a>
+            <a href="/dashboard" className="p-5 duration-300 ease-in-out hover:bg-[#304165] hover:text-white">Dashboard</a>
             <a href="/practice" className="p-5 duration-300 ease-in-out hover:bg-[#304165] hover:text-white">Practice</a>
-            <a href="/settings" className="p-5 duration-300 ease-in-out hover:bg-[#304165] hover:text-white">Settings</a>
             <a href="/profile" className="p-5 text-white ml-auto">Profile</a>
         </div>
     );
